5495b64ea8397a4cfd0af3dc1bc58e3d9fbda7d1
Tiala Young <tialayoung@gmail.com>
2023-08-14 16:29:28 -0600

    Display item names on List page

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,18 +1,16 @@
 import { ListItem } from '../components';
 
 export function List({ data }) {
+	const listItemsToDisplay = data.map((item) => {
+		return <ListItem key={item.id} name={item.name} />;
+	});
+
 	return (
 		<>
 			<p>
 				Hello from the <code>/list</code> page!
 			</p>
-			<ul>
-				{/**
-				 * TODO: write some JavaScript that renders the `data` array
-				 * using the `ListItem` component that's imported at the top
-				 * of this file.
-				 */}
-			</ul>
+			<ul>{listItemsToDisplay}</ul>
 		</>
 	);
 }

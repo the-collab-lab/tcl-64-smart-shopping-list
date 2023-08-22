f989566c28a11e5492cdab49eb09b3fea56fc7a4
Ismarji Workman <ismarjiw@protonmail.com>
2023-08-22 12:29:03 -0400

    update createNewList function to handle saving token to Firestore and allowing for grocery list data to be added in the future

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,12 +1,29 @@
 import './Home.css';
 import { useNavigate } from 'react-router-dom';
+import { collection, addDoc } from 'firebase/firestore';
+import { db } from '../api/config';
 
 export function Home({ createToken }) {
 	const navigate = useNavigate();
 
-	function createNewList() {
-		if (createToken()) {
+	// function createNewList() {
+	// 	if (createToken()) {
+	// 		navigate('/list');
+	// 	}
+	// }
+
+	async function createNewList() {
+		const token = createToken();
+		const groceryData = {
+			itemName: null,
+			buyTime: null,
+		};
+		try {
+			await addDoc(collection(db, 'tokens', token, 'groceryList'), groceryData);
+			console.log('Grocery list created for token: ', token);
 			navigate('/list');
+		} catch (error) {
+			console.error('Error creating grocery list: ', error);
 		}
 	}
 

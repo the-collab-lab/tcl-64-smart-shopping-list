ba74283c10e69d572c963655ec9f3a3911805035
satoshi-sh <satoss1108@gmail.com>
2023-08-28 12:34:20 -0500

    Made a form and filter function. Tested with console if we get right filtered list.

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,15 +1,39 @@
 import { ListItem } from '../components';
+import { useState } from 'react';
 
 export function List({ data }) {
-	const listItemsToDisplay = data.map((item) => {
+	const [searchInput, setSearchInput] = useState('');
+	const [displayList, setDisplayList] = useState(data);
+	const filterDisplay = (currentInput) => {
+		const filteredList = displayList.filter((listItem) => {
+			return listItem.name.toLowerCase().includes(currentInput.toLowerCase());
+		});
+		console.log(filteredList);
+	};
+
+	const listItemsToDisplay = displayList.map((item) => {
 		return <ListItem key={item.id} name={item.name} />;
 	});
-
+	console.log(searchInput);
 	return (
 		<>
 			<p>
 				Hello from the <code>/list</code> page!
 			</p>
+			<form>
+				<label htmlFor="filter">Filter by item name</label>
+				<br />
+				<input
+					type="text"
+					name="filter"
+					id="filter"
+					value={searchInput}
+					onChange={(e) => {
+						setSearchInput(e.target.value);
+						filterDisplay(searchInput);
+					}}
+				></input>
+			</form>
 			<ul>{listItemsToDisplay}</ul>
 		</>
 	);

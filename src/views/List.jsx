099fe64aa9510aed8393d54121703a159030a00a
satoshi-sh <satoss1108@gmail.com>
2023-09-04 16:59:44 -0500

    Added welcome prompt when the shopping list is empty.

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,8 +1,66 @@
 import { ListItem } from '../components';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export function List({ data }) {
 	const [searchInput, setSearchInput] = useState('');
+	const [isLoading, setIsLoading] = useState(true);
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		setTimeout(() => {
+			setIsLoading(false);
+		}, 1000);
+	}, []);
+
+	const WelcomPrompt = () => {
+		return (
+			<>
+				<h3>Shopping list is currently empty...</h3>
+				<button
+					type="button"
+					onClick={() => {
+						navigate('/add-item');
+					}}
+				>
+					Add your first item
+				</button>
+			</>
+		);
+	};
+	const FormandList = () => {
+		return (
+			<>
+				<form>
+					<label htmlFor="filter">Filter List</label>
+					<br />
+					<input
+						type="text"
+						name="filter"
+						id="filter"
+						value={searchInput}
+						placeholder="Item name"
+						onChange={(e) => {
+							setSearchInput(e.target.value);
+						}}
+						onKeyDown={(e) => {
+							handleKeyDown(e);
+						}}
+					></input>
+					<button
+						type="button"
+						name="clearInput"
+						onClick={(e) => {
+							resetDisplayList(e);
+						}}
+					>
+						X
+					</button>
+				</form>
+				<ul>{listItemsToDisplay}</ul>
+			</>
+		);
+	};
 
 	const listItemsToDisplay = data.map((item) => {
 		const isItemInSearch = item.name
@@ -25,33 +83,13 @@ export function List({ data }) {
 
 	return (
 		<>
-			<form>
-				<label htmlFor="filter">Filter List</label>
-				<br />
-				<input
-					type="text"
-					name="filter"
-					id="filter"
-					value={searchInput}
-					placeholder="Item name"
-					onChange={(e) => {
-						setSearchInput(e.target.value);
-					}}
-					onKeyDown={(e) => {
-						handleKeyDown(e);
-					}}
-				></input>
-				<button
-					type="button"
-					name="clearInput"
-					onClick={(e) => {
-						resetDisplayList(e);
-					}}
-				>
-					X
-				</button>
-			</form>
-			<ul>{listItemsToDisplay}</ul>
+			{isLoading ? (
+				<h3>Loading...</h3>
+			) : data.length > 1 ? (
+				<FormandList />
+			) : (
+				<WelcomPrompt />
+			)}
 		</>
 	);
 }

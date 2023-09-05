7333bf0f336c59176231a2ce1046093830a5bde9
Christina Woodhams <cwoodhams89@gmail.com>
2023-09-05 17:35:20 -0400

    add checkboxes

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,5 +1,19 @@
 import './ListItem.css';
+import { useState } from 'react';
 
 export function ListItem({ name }) {
-	return <li className="ListItem">{name}</li>;
+	const [isChecked, setIsChecked] = useState(false);
+
+	const handleCheck = async () => {
+		setIsChecked(true);
+	};
+
+	return (
+		<li className="ListItem">
+			<label>
+				<input type="checkbox" onChange={handleCheck} />
+				{name}
+			</label>
+		</li>
+	);
 }

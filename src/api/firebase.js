efaa5efde76a002b95cf5c25ba10f7e708f3e130
Tiala Young <tialayoung@gmail.com>
2023-09-19 18:54:11 -0600

    Write deleteItem function to delete an item from the firestore

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -5,6 +5,7 @@ import {
 	getDocs,
 	doc,
 	updateDoc,
+	deleteDoc,
 } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
 import { db } from './config';
@@ -118,12 +119,14 @@ export async function updateItem(
 	}
 }
 
-export async function deleteItem() {
-	/**
-	 * TODO: Fill this out so that it uses the correct Firestore function
-	 * to delete an existing item. You'll need to figure out what arguments
-	 * this function must accept!
-	 */
+export async function deleteItem(listId, itemId) {
+	const currentItemRef = doc(db, listId, itemId);
+
+	try {
+		await deleteDoc(currentItemRef);
+	} catch (error) {
+		console.error('Unable to delete item:', error.message);
+	}
 }
 
 export async function createNewList(listId) {

e6cd2acec4180654691abf56ef4d53ea16514823
satoshi-sh <satoss1108@gmail.com>
2023-10-01 20:32:55 -0500

    Utilized classnames library and set up dark mode colors

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import classnames from 'classnames';
 
-const Button = ({ text, variant, onClick, withIcon, className }) => {
+const Button = ({ text, variant, type, onClick, withIcon, className }) => {
 	const variantClasses = {
-		primary: 'bg-green text-white hover:bg-green-dark active:scale-105',
-		secondary: 'bg-gray-300 text-gray-700 hover:bg-gray-400',
+		primary:
+			'bg-green dark:bg-light-blue text-white hover:bg-green-dark active:scale-105 dark:text-black',
+		secondary:
+			'bg-gray-300 text-gray-600 hover:bg-gray-400 dark:text-white dark:bg-gray-700 active:scale-105',
 	};
 	const classes = classnames(
 		'px-32 py-4 rounded-full transition duration-300 focus:outline-none',
@@ -12,9 +14,9 @@ const Button = ({ text, variant, onClick, withIcon, className }) => {
 		className,
 	);
 	const iconSpan = <span className="text-5xl">+</span>;
-
+	const buttonType = type ? type : 'submit';
 	return (
-		<button type="submit" className={classes} onClick={onClick}>
+		<button type={buttonType} className={classes} onClick={onClick}>
 			{withIcon ? (
 				<div className="flex items-center gap-4">
 					{text} {iconSpan}

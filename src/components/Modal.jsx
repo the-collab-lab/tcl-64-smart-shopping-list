97032f3c09db7c927915ff7194f68d75d777a805
Tiala Young <tialayoung@gmail.com>
2023-10-02 21:43:21 -0600

    Finish logout modal functionality

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
 	TERipple,
 	TEModal,
@@ -9,22 +9,9 @@ import {
 	TEModalFooter,
 } from 'tw-elements-react';
 
-export function Modal() {
-	const [showModal, setShowModal] = useState(false);
-
+export function Modal({ showModal, setShowModal, confirmationAction }) {
 	return (
 		<div>
-			{/* <!-- Button trigger modal --> */}
-			<TERipple rippleColor="white">
-				<button
-					type="button"
-					className="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-					onClick={() => setShowModal(true)}
-				>
-					Launch demo modal
-				</button>
-			</TERipple>
-
 			{/* <!-- Modal --> */}
 			<TEModal show={showModal} setShow={setShowModal}>
 				<TEModalDialog>
@@ -58,7 +45,7 @@ export function Modal() {
 							</button>
 						</TEModalHeader>
 						{/* <!--Modal body--> */}
-						<TEModalBody>Modal body text goes here.</TEModalBody>
+						<TEModalBody>Are you sure you want to leave this list?</TEModalBody>
 						<TEModalFooter>
 							<TERipple rippleColor="light">
 								<button
@@ -66,15 +53,16 @@ export function Modal() {
 									className="inline-block rounded bg-primary-100 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200"
 									onClick={() => setShowModal(false)}
 								>
-									Close
+									Cancel
 								</button>
 							</TERipple>
 							<TERipple rippleColor="light">
 								<button
 									type="button"
 									className="ml-1 inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+									onClick={confirmationAction}
 								>
-									Save changes
+									Confirm
 								</button>
 							</TERipple>
 						</TEModalFooter>
--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -1,4 +1,5 @@
 import { Outlet, NavLink, useLocation } from 'react-router-dom';
+import { Modal } from '../components/Modal';
 import './Layout.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
@@ -7,25 +8,14 @@ import {
 	faPlus,
 } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { Modal } from '../components/Modal';
-import { useState } from 'react';
-import {
-	TERipple,
-	TEModal,
-	TEModalDialog,
-	TEModalContent,
-	TEModalHeader,
-	TEModalBody,
-	TEModalFooter,
-} from 'tw-elements-react';
 
-export function Layout({ setListToken }) {
-	const [showModal, setShowModal] = useState(false);
+export function Layout({ setListToken, showModal, setShowModal }) {
 	const location = useLocation();
 
 	const removeListToken = () => {
 		localStorage.removeItem('tcl-shopping-list-token');
 		setListToken(null);
+		setShowModal(false);
 	};
 
 	const listToken = localStorage.getItem('tcl-shopping-list-token');
@@ -59,104 +49,56 @@ export function Layout({ setListToken }) {
 		} else {
 			if (listToken) {
 				return (
-					<div className="Nav-container -mt-4">
-						<NavLink
-							to="/list"
-							className={`Nav-link ${
-								location.pathname === '/list' ? 'underline text-black' : ''
-							}`}
-						>
-							<FontAwesomeIcon
-								icon={faList}
-								title="Navigate to list page"
-								className="text-black"
-							/>
-							<br />
-							<p className="text-lg text-black">VIEW LIST</p>
-						</NavLink>
-						<NavLink
-							to="/add-item"
-							className={`Nav-link ${
-								location.pathname === '/add-item' ? 'underline text-black' : ''
-							}`}
-						>
-							<FontAwesomeIcon
-								icon={faPlus}
-								title="Navigate to the add item page"
-								className="text-black"
-							/>
-							<br />
-							<p className="text-lg text-black">ADD ITEM</p>
-						</NavLink>
-						<NavLink className="Nav-link">
-							<FontAwesomeIcon
-								icon={faRightFromBracket}
-								title="Leave list"
-								className="text-black"
-								type="button"
-								// onClick={removeListToken}
-								onClick={() => setShowModal(true)}
-							/>
-							<p className="text-lg text-black">LEAVE LIST</p>
-							<br />
-						</NavLink>
-
-						{/* <TEModal show={showModal} setShow={setShowModal}>
-				<TEModalDialog>
-					<TEModalContent>
-						<TEModalHeader>
-							
-							<h5 className="text-xl font-medium leading-normal text-neutral-800 dark:text-neutral-200">
-								Modal title
-							</h5>
-							
-							<button
-								type="button"
-								className="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
-								onClick={() => setShowModal(false)}
-								aria-label="Close"
+					<>
+						<div className="Nav-container -mt-4">
+							<NavLink
+								to="/list"
+								className={`Nav-link ${
+									location.pathname === '/list' ? 'underline text-black' : ''
+								}`}
 							>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									strokeWidth="1.5"
-									stroke="currentColor"
-									className="h-6 w-6"
-								>
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										d="M6 18L18 6M6 6l12 12"
-									/>
-								</svg>
-							</button>
-						</TEModalHeader>
-						
-						<TEModalBody>Modal body text goes here.</TEModalBody>
-						<TEModalFooter>
-							<TERipple rippleColor="light">
-								<button
+								<FontAwesomeIcon
+									icon={faList}
+									title="Navigate to list page"
+									className="text-black"
+								/>
+								<br />
+								<p className="text-lg text-black">VIEW LIST</p>
+							</NavLink>
+							<NavLink
+								to="/add-item"
+								className={`Nav-link ${
+									location.pathname === '/add-item'
+										? 'underline text-black'
+										: ''
+								}`}
+							>
+								<FontAwesomeIcon
+									icon={faPlus}
+									title="Navigate to the add item page"
+									className="text-black"
+								/>
+								<br />
+								<p className="text-lg text-black">ADD ITEM</p>
+							</NavLink>
+							<NavLink className="Nav-link">
+								<FontAwesomeIcon
+									icon={faRightFromBracket}
+									title="Leave list"
+									className="text-black"
 									type="button"
-									className="inline-block rounded bg-primary-100 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200"
-									onClick={() => setShowModal(false)}
-								>
-									Close
-								</button>
-							</TERipple>
-							<TERipple rippleColor="light">
-								<button
-									type="button"
-									className="ml-1 inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-								>
-									Save changes
-								</button>
-							</TERipple>
-						</TEModalFooter>
-					</TEModalContent>
-				</TEModalDialog>
-			</TEModal> */}
-					</div>
+									onClick={() => setShowModal(true)}
+								/>
+								<p className="text-lg text-black">LEAVE LIST</p>
+								<br />
+							</NavLink>
+						</div>
+						<Modal
+							showModal={showModal}
+							setShowModal={setShowModal}
+							confirmationAction={removeListToken}
+						/>
+					</>
 				);
 			}
 		}

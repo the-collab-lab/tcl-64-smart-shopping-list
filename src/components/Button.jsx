47da6443fe41fb96fea78acec475227e5cf2e34d
satoshi-sh <satoss1108@gmail.com>
2023-10-04 07:28:26 -0500

    Added secondaryConfirm and secondaryCancel for modal

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,12 +6,14 @@ import { faPlus as plus } from '@fortawesome/free-solid-svg-icons';
 const Button = ({ text, variant, type, onClick, withIcon, className }) => {
 	const variantClasses = {
 		primary:
-			'bg-green dark:bg-light-blue text-white hover:bg-green-dark active:scale-105 dark:text-black',
-		secondary:
-			'bg-gray-300 text-gray-600 hover:bg-gray-400 dark:text-white dark:bg-gray-700 active:scale-105',
+			'bg-green dark:bg-light-blue text-white hover:bg-green-dark active:scale-105 dark:text-black px-32 py-4',
+		secondaryConfirm:
+			'bg-light-blue border dark:bg-light-blue dark:border-hidden dark:text-black text-black hover:bg-green-dark dark:bg-gray-700 active:scale-105 px-24 py-4',
+		secondaryCancel:
+			'bg-white border text-black hover:bg-gray-400 dark:text-white dark:bg-gray-700 active:scale-105 px-16 py-4',
 	};
 	const classes = classnames(
-		'px-32 py-4 rounded-full transition duration-300 focus:outline-none',
+		'rounded-full transition duration-300 focus:outline-none',
 		variantClasses[variant] || variantClasses.primary,
 		className,
 	);
